91f94c04db522f4fd99c265f1a040c97f2a62788
knipferrc <knipferrc@gmail.com>
2022-02-28 09:06:15 -0500

    remove wrapper around view

--- internal/tui/view.go
+++ internal/tui/view.go
@@ -1,11 +1,6 @@
 package tui
 
-import "github.com/charmbracelet/lipgloss"
-
 // View returns a string representation of the entire application UI.
 func (b Bubble) View() string {
-	return lipgloss.NewStyle().
-		Width(b.screenWidth).
-		Height(b.screenHeight).
-		Render(b.list.View())
+	return b.list.View()
 }
