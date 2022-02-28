b930d4c9d678eb79e1de8159709edc33639ddf20
knipferrc <knipferrc@gmail.com>
2022-02-28 08:25:15 -0500

    cleanup init

--- internal/tui/init.go
+++ internal/tui/init.go
@@ -1,16 +1,10 @@
 package tui
 
 import (
-	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 // Init initializes the UI.
 func (b Bubble) Init() tea.Cmd {
-	var cmds []tea.Cmd
-
-	cmds = append(cmds, spinner.Tick)
-	cmds = append(cmds, readCurrentGitBranchesCmd())
-
-	return tea.Batch(cmds...)
+	return tea.Batch(b.list.StartSpinner(), readCurrentGitBranchesCmd())
 }
