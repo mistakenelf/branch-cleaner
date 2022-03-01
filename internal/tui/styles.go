1ed9703730c8f52f6d77da7244c0d6b1b24e7a59
knipferrc <knipferrc@gmail.com>
2022-03-01 12:51:23 -0500

    update styles

--- internal/tui/styles.go
+++ internal/tui/styles.go
@@ -3,8 +3,16 @@ package tui
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	listStyle          = lipgloss.NewStyle().Margin(1)
-	statusMessageStyle = lipgloss.NewStyle().
+	listStyle              = lipgloss.NewStyle().Margin(1)
+	statusMessageInfoStyle = lipgloss.NewStyle().
 				Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#04B575"}).
 				Render
+	statusMessageErrorStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.AdaptiveColor{Light: "#FF0000", Dark: "#FF0000"}).
+				Render
+)
+
+const (
+	iconSelected    = "●"
+	iconNotSelected = "○"
 )
